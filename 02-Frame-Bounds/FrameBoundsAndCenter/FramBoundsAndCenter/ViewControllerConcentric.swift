d5259517672cb53afb54afd160f3569549e35e94
SlickJohnson <liwa.johnson@gmail.com>
2018-05-02 15:53:09 -0700

    Complete concentric view challenge

--- 02-Frame-Bounds/FrameBoundsAndCenter/FramBoundsAndCenter/ViewControllerConcentric.swift
+++ 02-Frame-Bounds/FrameBoundsAndCenter/FramBoundsAndCenter/ViewControllerConcentric.swift
@@ -12,12 +12,13 @@ class ViewControllerConcentric: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .red
-    let numberOfViews = 14
-    for i in stride(from: CGFloat(numberOfViews), to: 0.0, by: -1) {
+    let numberOfViews = 20
+    for i in stride(from: CGFloat(numberOfViews), to: 0.0, by: -1.0) {
       let r = view.bounds.insetBy(dx: CGFloat(10 * i), dy: CGFloat(10 * i))
       let box = UIView(frame: r)
       view.insertSubview(box, at: 0)
-      box.backgroundColor = UIColor(hue: 1, saturation: saturation, brightness: 1, alpha: 1)
+      let saturation = 1 - (i / CGFloat(numberOfViews))
+      box.backgroundColor = UIColor(hue: 1, saturation: CGFloat(saturation), brightness: 1, alpha: 1)
     }
   }
   
